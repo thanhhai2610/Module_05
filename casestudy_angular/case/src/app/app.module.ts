74b5795b4fdde5c54a45e602a6e5944200661a71
thanhhai2610 <thanhai707726@gmail.com>
2022-11-22 16:26:19 +0700

    case

--- casestudy_angular/case/src/app/app.module.ts
+++ casestudy_angular/case/src/app/app.module.ts
@@ -3,11 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { FacilityComponent } from './facility/facility.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { NavComponent } from './nav/nav.component';
+import { EditFacilityComponent } from './edit-facility/edit-facility.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    FacilityComponent,
+    HeaderComponent,
+    FooterComponent,
+    NavComponent,
+    EditFacilityComponent
   ],
   imports: [
     BrowserModule
